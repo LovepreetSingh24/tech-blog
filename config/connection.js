fc4c7e1588dc77b986e3022bba4d9307bac08442
LovepreetSingh24 <lovepreet.singh@att.net>
2024-01-28 16:33:35 -0800

    added sql file

--- config/connection.js
+++ config/connection.js
@@ -8,3 +8,18 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 });
 
 module.exports = sequelize;
+
+const mysql = require('mysql2');
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+      host: 'localhost',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+  });
+  
+}
